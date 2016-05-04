5f35dadc34dfe0336cd9fffa2d7f254881236a62
Jon <jambler24@gmail.com>
2016-05-04 16:18:24 +0200

    Worked on the import of fasta files

---
+++ extract_seq.py
@@ -0,0 +1,34 @@
+
+
+
+# User set parameters:
+
+fasta_filepath = ""
+
+gff_filepath = ""
+
+gene_name = ""
+
+
+# Needed functions 
+
+def import_fasta():
+	print "working here"
+
+def import_gff():
+
+
+def retrieve_seq():
+
+
+# Main part of the script
+
+
+fasta_dict = import_fasta()
+
+
+gff_coordinates = import_gff()
+
+
+print retrieve_seq(fasta_dict, gff_coordinates, gene_name)
+
